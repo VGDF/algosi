8359df269a7c737ba2e4af8136926de1a33886f1
VGDF <tchernobilsky.lenya@yandex.ru>
2025-10-28 01:20:12 +0300

    Initial commit

---
+++ sorting/include/optimized_bubble_sort.h
@@ -0,0 +1,8 @@
+#ifndef OPTIMIZED_BUBBLE_SORT_H
+#define OPTIMIZED_BUBBLE_SORT_H
+
+template <typename T>
+void optimizedBubbleSort(T arr[], int n);
+
+
+#endif // OPTIMIZED_BUBBLE_SORT_H
---
+++ sorting/include/selection_sort.h
@@ -0,0 +1,8 @@
+#ifndef SELECTION_SORT_H
+#define SELECTION_SORT_H
+
+template <typename T>
+void selectionSort(T arr[], int n);
+
+
+#endif // SELECTION_SORT_H
