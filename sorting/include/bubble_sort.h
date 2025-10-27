558b17915a34bb5076a43602b463773f52213403
VGDF <74319845+VGDF@users.noreply.github.com>
2025-10-28 01:11:02 +0300

    Delete sorting directory

--- sorting/include/bubble_sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef BUBBLE_SORT_H
-#define BUBBLE_SORT_H
-
-template <typename T>
-void bubbleSort(T arr[], int n);
-
-
-#endif // BUBBLE_SORT_H
--- sorting/include/selection_sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef SELECTION_SORT_H
-#define SELECTION_SORT_H
-
-template <typename T>
-void selectionSort(T arr[], int n);
-
-
-#endif // SELECTION_SORT_H
