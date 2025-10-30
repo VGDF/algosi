d017cc60b8e816921de57760b51ef815a0fe9869
VGDF <tchernobilsky.lenya@yandex.ru>
2025-10-30 14:33:26 +0300

    Commit all changes in the ai folder

--- laba1/git_push.bat
+++ laba1/git_push.bat
@@ -2,7 +2,7 @@
 cd c:\Users\user\Desktop\ai
 git init
 git add laba1
-git add seminar1
+git add "seminar 1"
 git commit -m "Initial commit"
 git remote add origin https://github.com/VGDF/algosi
 git push -u origin main
