c5ed7e2f929aefcb472a7205d3f7ed072733b78e
VGDF <tchernobilsky.lenya@yandex.ru>
2025-10-30 13:31:24 +0300

    delete Leo_Che,seminar 1, googletest

--- seminar 1/include/exchange_sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXCHANGE_SORT_H
-#define EXCHANGE_SORT_H
-
-template <typename T>
-void exchangeSort(T arr[], int n);
-
-
-#endif // EXCHANGE_SORT_H
--- seminar 1/include/optimized_bubble_sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef OPTIMIZED_BUBBLE_SORT_H
-#define OPTIMIZED_BUBBLE_SORT_H
-
-template <typename T>
-void optimizedBubbleSort(T arr[], int n);
-
-
-#endif // OPTIMIZED_BUBBLE_SORT_H
