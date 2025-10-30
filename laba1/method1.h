1351c772010620b3c5875c842a9030b5f708a03b
VGDF <74319845+VGDF@users.noreply.github.com>
2025-10-30 20:12:05 +0300

    Delete laba1 directory

--- laba1/method1.h
+++
@@ -1,6 +0,0 @@
-#ifndef METHOD1_H
-#define METHOD1_H
-
-// Function declarations for method1
-
-#endif // METHOD1_H
--- laba1/method2.h
+++
@@ -1,6 +0,0 @@
-#ifndef METHOD2_H
-#define METHOD2_H
-
-// Function declarations for method2
-
-#endif // METHOD2_H
